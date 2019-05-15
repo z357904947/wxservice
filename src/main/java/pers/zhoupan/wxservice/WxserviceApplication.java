6dedb9ae74449b3b61e7c51ffc151a72870c78b7
zhoupan <z357904947@126.com>
2019-05-15 14:17:31 +0800

    第一次构建项目提交。

---
+++ src/main/java/pers/zhoupan/wxservice/WxserviceApplication.java
@@ -0,0 +1,13 @@
+package pers.zhoupan.wxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxserviceApplication.class, args);
+    }
+
+}
